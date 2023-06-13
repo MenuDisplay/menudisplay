6f68354f6c2ff7dd942f5240485e41851eb1f795
Jacob Hawkins <jacobhawkins7@gmail.com>
2023-06-13 12:52:09 -0700

    fullscreen functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return <img src='/Service List 6-8-2023.jpg' alt='servicesMenu'></img>;
-}
+const App = () => {
+  const handleImageClick = (event) => {
+    const imgElement = event.target;
+    if (imgElement.requestFullscreen) {
+      imgElement.requestFullscreen();
+    } else if (imgElement.mozRequestFullScreen) {
+      imgElement.mozRequestFullScreen();
+    } else if (imgElement.webkitRequestFullscreen) {
+      imgElement.webkitRequestFullscreen();
+    } else if (imgElement.msRequestFullscreen) {
+      imgElement.msRequestFullscreen();
+    }
+  };
+
+  return (
+    <div className='App'>
+      <img
+        src='/Service List 6-8-2023.jpg'
+        alt='serviceMenu'
+        onClick={handleImageClick}
+      />
+    </div>
+  );
+};
 
 export default App;
